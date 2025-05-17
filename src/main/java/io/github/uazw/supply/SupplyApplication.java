17965c2555e268e672e7193d71ae5c69560c17c2
tofu <uazw@foxmail.com>
2025-05-17 14:41:17 +0800

    project init.

---
+++ src/main/java/io/github/uazw/supply/SupplyApplication.java
@@ -0,0 +1,13 @@
+package io.github.uazw.supply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/uazw/supply/SupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.uazw.supply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
