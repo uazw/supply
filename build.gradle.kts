5b7b53f6092d389bf70f61ce1a8db9e036880f7b
tofu <uazw@foxmail.com>
2025-05-19 22:18:10 +0800

    dockerized & add more script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,74 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "3.4.5"
+  id("io.spring.dependency-management") version "1.1.7"
+  checkstyle
+  jacoco
 }
 
 group = "io.github.uazw"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
+}
+
+checkstyle {
+  toolVersion = "10.23.1"
+}
+
+jacoco {
+  toolVersion = "0.8.12"
+}
+tasks.jacocoTestReport {
+  reports {
+    xml.required = false
+    csv.required = false
+  }
+  dependsOn(tasks.test)
+  finalizedBy(tasks.jacocoTestCoverageVerification)
+  classDirectories.setFrom(
+    sourceSets.main.get().output.asFileTree.matching {
+      exclude("io/github/uazw/supply/SupplyApplication*")
+    }
+  )
+}
+tasks.jacocoTestCoverageVerification {
+  violationRules {
+    rule {
+      classDirectories.setFrom(sourceSets.main.get().output.asFileTree.matching {
+        exclude("io/github/uazw/supply/SupplyApplication*")
+      })
+      limit {
+        minimum = "0.8".toBigDecimal()
+      }
+
+    }
+  }
+}
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.flywaydb:flyway-core")
+  implementation("org.flywaydb:flyway-database-postgresql")
+  runtimeOnly("org.postgresql:postgresql")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
--- src/main/java/io/github/uazw/supply/SupplyApplication.java
+++ src/main/java/io/github/uazw/supply/SupplyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SupplyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SupplyApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SupplyApplication.class, args);
+  }
 
 }
